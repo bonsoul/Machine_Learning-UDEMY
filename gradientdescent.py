7b2041f6bf95e3367f090bf1631302fbe02f3cf7
bonsoul <bonsoul24@gmail.com>
2025-09-11 20:51:47 +0300

    Gradient

--- gradientdescent.py
+++ gradientdescent.py
@@ -76,14 +76,14 @@ def mse_loss(y_true, y_predict):
 model = LinearModel()
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
 
-#for epoch in range(100):
-    #with tf.GradientTape() as tape:
-        #y_pred = model(X_tensor)
-        #loss = mse_loss(y_tensor, y_pred)
-    #gradients = tape.gradient(loss, [model.weights, model.bias])
-    #optimizer.apply_gradients(zip(gradients, [model.weights, model.bias]))
-    #if epoch % 10 == 0:
-        #print(f"Epoch {epoch}, Loss: {loss.numpy():.4f}")
+for epoch in range(100):
+    with tf.GradientTape() as tape:
+        y_pred = model(X_tensor)
+        loss = mse_loss(y_tensor, y_pred)
+    gradients = tape.gradient(loss, [model.weights, model.bias])
+    optimizer.apply_gradients(zip(gradients, [model.weights, model.bias]))
+    if epoch % 10 == 0:
+        print(f"Epoch {epoch}, Loss: {loss.numpy():.4f}")
 
 
 
