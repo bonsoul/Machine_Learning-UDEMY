baddb7dfefd5e67f755b9a9dab5b698cbd787db3
bonsoul <bonsoul24@gmail.com>
2025-09-16 12:08:50 +0300

    Pool Layers

--- pool_layers.py
+++ pool_layers.py
@@ -19,14 +19,14 @@ max_pooled = maximum_filter(feature_map, size=2, mode='constant')
 avg_pooled = uniform_filter(feature_map, size=2, mode="constant")
 
 #plot
-#fig,axes = plt.subplots(1, 3, figsize=(12,4))
-#axes[0].imshow(feature_map, cmap="viridis")
-#axes[0].set_title("Original Feature Map")
-#axes[1].imshow(max_pooled, cmap="viridis")
-#axes[1].set_title("Max_Pooled")
-#axes[2].imshow(avg_pooled, cmap="viridis")
-#axes[2].set_title("Average Pooled")
-#plt.show()
+fig,axes = plt.subplots(1, 3, figsize=(12,4))
+axes[0].imshow(feature_map, cmap="viridis")
+axes[0].set_title("Original Feature Map")
+axes[1].imshow(max_pooled, cmap="viridis")
+axes[1].set_title("Max_Pooled")
+axes[2].imshow(avg_pooled, cmap="viridis")
+axes[2].set_title("Average Pooled")
+plt.show()
 
 
 
@@ -34,21 +34,21 @@ import tensorflow as tf
 
 #create a smaple input tensor (1x4x4x4)
 
-#input_tensor = tf.constant(feature_map.reshape(1, 4, 4, 1), dtype=tf.float32)
+input_tensor = tf.constant(feature_map.reshape(1, 4, 4, 1), dtype=tf.float32)
 
 #max pooling
 
-#max_pool = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2, padding='valid')
-#max_pooled_tensor = max_pool(input_tensor)
+max_pool = tf.keras.layers.MaxPooling2D(pool_size=(2,2), strides=2, padding='valid')
+max_pooled_tensor = max_pool(input_tensor)
 
 
-#avg pooling
-#avg_pool = tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=2, padding='valid')
-#avg_pooled_tensor = avg_pool(input_tensor)
+avg pooling
+avg_pool = tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=2, padding='valid')
+avg_pooled_tensor = avg_pool(input_tensor)
 
-#print(f"Max Pooled Tensor: \n{tf.squeeze(max_pooled_tensor).numpy()}")
-#print(f"Average Pooled Tensor: \n{tf.squeeze(avg_pooled_tensor).numpy}")
-#print("\n\n\n")
+print(f"Max Pooled Tensor: \n{tf.squeeze(max_pooled_tensor).numpy()}")
+print(f"Average Pooled Tensor: \n{tf.squeeze(avg_pooled_tensor).numpy}")
+print("\n\n\n")
 
 #pytorch
 
